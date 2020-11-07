9b5f066ddcc59814c3a02e04f24babd697cd7c8d
juefeiyan <yjf27281181@gmail.com>
2020-11-07 14:47:07 -0800

    init project

---
+++ cloud-provider-payment8001/src/main/java/com/amazon/cat/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.amazon.cat.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yanjuefei
+ * @date 2020/11/7 0007
+ * @Description
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
